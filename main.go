f5eab3454df2513fbe6f6660431a07c919e3894e
Addy Darmawan <addy.darmawan@kudo.co.id>
2018-04-19 14:17:29 +0700

    Suffle

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
 	// hand, remainingCards := deal(cards, 5)
 
 	// hand.print()
 	// remainingCards.print()
 
-	fmt.Println(cards.toString())
+	// fmt.Println(cards.toString())
+	deck := newDeck()
+	deck.shuffle()
+	deck.print()
+	// cards.saveToFile("halo")
 }
 
 func newCard() string {
